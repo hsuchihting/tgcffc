d4ffd0e4ff78135c858b6ed64ec6e1b3a6a65794
Tim Hsu <tim.hsu@tpisoftware.com>
2023-12-13 23:41:57 +0800

    Update Vite configuration and remove vue.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from 'node:url';
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: "/tgcffc/",
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {   
+    outDir: 'dist',
+  },
+});
